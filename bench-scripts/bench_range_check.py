8ec639bdc709cd648b945cb0119e66e008fda6e9
Ubuntu <ubuntu@ip-172-31-26-105.us-west-2.compute.internal>
2023-03-06 04:27:58 +0000

    Update range check script.

--- bench-scripts/bench_range_check.py
+++ bench-scripts/bench_range_check.py
@@ -7,12 +7,13 @@ subprocess.run(["make"])
 
 print("running the bench_range_check")
 
-num_records = 100000
-print("running the case for " + str(num_records) + " entries")
+num_records_list = [100000, 200000, 500000]
 
-time.sleep(5)
-for i in range(5):
-    range = pow(2, 8 + 4 * i)
-    write_configure_info(str(num_records) + " " + str(range))
-    subprocess.run(["bin/bench_range_check", os.getenv("EMP_MY_PARTY_ID"), "5000"])
-    copy_benchmark_result_to_log("bench_range_check " + str(num_records) + " " + str(range))
+for num_records in num_records_list:
+    print("running the case for " + str(num_records) + " entries")
+    time.sleep(5)
+    for i in range(5):
+        range_size = pow(2, 8 + 4 * i)
+        write_configure_info(str(num_records) + " " + str(range_size))
+        subprocess.run(["bin/bench_range_check", os.getenv("EMP_MY_PARTY_ID"), "5000"])
+        copy_benchmark_result_to_log("bench_range_check " + str(num_records) + " " + str(range_size))
