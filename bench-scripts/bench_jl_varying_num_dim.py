7064c16a14e960c75ae1e3abb90ecdd44862369c
Ubuntu <ubuntu@ip-172-31-26-105.us-west-2.compute.internal>
2023-03-06 10:10:12 +0000

    Fixed k value for strawman tests.

--- bench-scripts/bench_jl_varying_num_dim.py
+++ bench-scripts/bench_jl_varying_num_dim.py
@@ -8,13 +8,14 @@ subprocess.run(["make"])
 print("running the bench_jl_varying_num_dim")
 
 k = 200
-num_records = 100000
+num_records_list = [100000, 200000, 500000]
 size_of_each_dimension = 10
-print("running the case for " + str(num_records) + " entries, with k value " + str(k) + ", and each dimension has size " + str(size_of_each_dimension))
 
-time.sleep(5)
-for i in range(5):
-    num_dimensions = 1 + i * 1
-    write_configure_info(str(num_dimensions) + " " + str(size_of_each_dimension) + " " + str(k) + " " + str(num_records))
-    subprocess.run(["bin/bench_jl", os.getenv("EMP_MY_PARTY_ID"), "5000"])
-    copy_benchmark_result_to_log("bench_jl varying " + str(num_dimensions) + " " + str(size_of_each_dimension) + " " + str(k) + " " + str(num_records))
+for num_records in num_records_list:
+    print("running the case for " + str(num_records) + " entries, with k value " + str(k) + ", and each dimension has size " + str(size_of_each_dimension))
+    time.sleep(5)
+    for i in range(5):
+        num_dimensions = 1 + i * 1
+        write_configure_info(str(num_dimensions) + " " + str(size_of_each_dimension) + " " + str(k) + " " + str(num_records))
+        subprocess.run(["bin/bench_jl", os.getenv("EMP_MY_PARTY_ID"), "5000"])
+        copy_benchmark_result_to_log("bench_jl varying " + str(num_dimensions) + " " + str(size_of_each_dimension) + " " + str(k) + " " + str(num_records))
--- bench-scripts/bench_jl_varying_size_dim.py
+++ bench-scripts/bench_jl_varying_size_dim.py
@@ -8,13 +8,14 @@ subprocess.run(["make"])
 print("running the bench_jl_varying_size_dim")
 
 k = 200
-num_records = 100000
+num_records_list = [100000, 200000, 500000]
 num_dimensions = 4
-print("running the case for " + str(num_records) + " entries, with k value " + str(k) + ", and " + str(num_dimensions) + " dimensions")
 
-time.sleep(5)
-for i in range(5):
-    size_of_each_dimension = 5 + 5 * i
-    write_configure_info(str(num_dimensions) + " " + str(size_of_each_dimension) + " " + str(k) + " " + str(num_records))
-    subprocess.run(["bin/bench_jl", os.getenv("EMP_MY_PARTY_ID"), "5000"])
-    copy_benchmark_result_to_log("bench_jl " + str(num_dimensions) + " varying " + str(size_of_each_dimension) + " " + str(k) + " " + str(num_records))
+for num_records in num_records_list:
+    print("running the case for " + str(num_records) + " entries, with k value " + str(k) + ", and " + str(num_dimensions) + " dimensions")
+    time.sleep(5)
+    for i in range(5):
+        size_of_each_dimension = 5 + 5 * i
+        write_configure_info(str(num_dimensions) + " " + str(size_of_each_dimension) + " " + str(k) + " " + str(num_records))
+        subprocess.run(["bin/bench_jl", os.getenv("EMP_MY_PARTY_ID"), "5000"])
+        copy_benchmark_result_to_log("bench_jl " + str(num_dimensions) + " varying " + str(size_of_each_dimension) + " " + str(k) + " " + str(num_records))
