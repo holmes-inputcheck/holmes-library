ae8098a7e7ea4192043fe8ce8da5562a01f38061
Ubuntu <ubuntu@ip-172-31-28-30.us-west-2.compute.internal>
2023-03-06 09:39:46 +0000

    Get the histogram and trimmed scripts ready.

--- bench-scripts/bench_histogram_numeric.py
+++ bench-scripts/bench_histogram_numeric.py
@@ -7,14 +7,12 @@ subprocess.run(["make"])
 
 print("running the bench_histogram_numeric")
 
-num_records = 100000
-num_groups = 10
-
-print("running the case for " + str(num_records) + " entries and " + str(num_groups) + "groups")
-
-time.sleep(5)
-for i in range(5):
-    range = pow(2, 4 + 2 * i)
-    write_configure_info(str(num_records) + " " + str(num_groups) + " " + str(range))
-    subprocess.run(["bin/bench_histogram_numeric", os.getenv("EMP_MY_PARTY_ID"), "5000"])
-    copy_benchmark_result_to_log("bench_histogram_numeric " + str(num_records) + " " + str(num_groups) + " " + str(range))
+record_group_tuple = [(100000, 10), (200000, 10), (200000, 20)]
+for num_records, num_groups in record_group_tuple:
+    print("running the case for " + str(num_records) + " entries and " + str(num_groups) + "groups")
+    time.sleep(5)
+    for i in range(5):
+        range_size = pow(2, 4 + 2 * i)
+        write_configure_info(str(num_records) + " " + str(num_groups) + " " + str(range_size))
+        subprocess.run(["bin/bench_histogram_numeric", os.getenv("EMP_MY_PARTY_ID"), "5000"])
+        copy_benchmark_result_to_log("bench_histogram_numeric " + str(num_records) + " " + str(num_groups) + " " + str(range_size))
--- bench-scripts/bench_trimmed_mean.py
+++ bench-scripts/bench_trimmed_mean.py
@@ -8,12 +8,13 @@ subprocess.run(["make"])
 print("running the bench_trimmed_mean")
 
 fixed_point_shift = 100
-num_records = 100000
-print("running the case for " + str(num_records) + " entries with a fixed-point shift of " + str(fixed_point_shift))
+num_records_list = [100000, 200000]
 
-time.sleep(5)
-for i in range(5):
-    range = pow(2, 8 + 4 * i)
-    write_configure_info(str(num_records) + " " + str(fixed_point_shift) + " " + str(range))
-    subprocess.run(["bin/bench_trimmed_mean", os.getenv("EMP_MY_PARTY_ID"), "5000"])
-    copy_benchmark_result_to_log("bench_trimmed_mean " + str(num_records) + " " + str(fixed_point_shift) + " " + str(range))
+for num_records in num_records_list:
+    print("running the case for " + str(num_records) + " entries with a fixed-point shift of " + str(fixed_point_shift))
+    time.sleep(5)
+    for i in range(5):
+        range_size = pow(2, 8 + 4 * i)
+        write_configure_info(str(num_records) + " " + str(fixed_point_shift) + " " + str(range_size))
+        subprocess.run(["bin/bench_trimmed_mean", os.getenv("EMP_MY_PARTY_ID"), "5000"])
+        copy_benchmark_result_to_log("bench_trimmed_mean " + str(num_records) + " " + str(fixed_point_shift) + " " + str(range_size))
